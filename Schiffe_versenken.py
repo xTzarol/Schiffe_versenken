2d481037253ef92148e3f1cd2cdcae7c38272715
xTzarol <leonhard.schreiner78@gmail.com>
2021-04-28 19:00:00 +0200

    Spielfeld funkt

--- Schiffe_versenken.py
+++ Schiffe_versenken.py
@@ -1,1 +1,43 @@
 #erste (zweite) Version des Programms
+from tkinter import *
+class GUI(Frame):
+    def __init__(self, master):
+        super().__init__(master) 
+        master.geometry("500x550")
+        #frame
+        self.f1 = Frame(master=master)
+        self.f1.pack(fill=BOTH, expand=True)
+        #game board size
+        self.grid_length = 10
+        self.create_board()
+
+        #make the grid expandable
+        for x in range(self.grid_length):
+            self.f1.columnconfigure(x, weight=1)
+            self.f1.rowconfigure(x, weight=1)
+
+    def create_board(self):
+        for x in range(self.grid_length):
+            for y in range(self.grid_length):
+                b = Button(master=self.f1,text="{}/{}".format(x,y))
+                b.grid(row=y, column=x, sticky=N+S+E+W)
+
+        for x in range(self.grid_length):
+            for y in range(self.grid_length, self.grid_length + 1):
+                l = Label(master=self.f1, bg = "grey", height = "1")
+                l.grid(row=y, column=x, sticky=N+S+E+W)
+
+        for x in range(self.grid_length):
+            for y in range(self.grid_length + 1, self.grid_length + 11):
+                b = Button(master=self.f1,text="{}/{}".format(x,y - 11))
+                b.grid(row=y, column=x, sticky=N+S+E+W)
+
+
+if __name__ == '__main__':
+
+    tk_window = Tk()
+    tk_window.title("Schiffe versenken")
+    app = GUI(tk_window)
+    app.mainloop()
+
+    #Command für Rückgabe wenn auf Button gedrückt wird
